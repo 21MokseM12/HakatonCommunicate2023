70659055a29dc7c16a9c504835db12084ec40132
21MokseM12 <maksim.buynov.00@mail.ru>
2023-11-10 13:34:18 +0300

    configuration commit

--- Hakaton2023/src/main/java/org/spring/mvcapp/App.java
+++ Hakaton2023/src/main/java/org/spring/mvcapp/App.java
@@ -1,17 +1,11 @@
 package org.spring.mvcapp;
 
-import config.DataPreparation;
-
-import java.io.IOException;
-
-/**
- * Hello world!
- *
- */
-
 public class App{
-    public static void main( String[] args ) {
-        DataPreparation.setParameters();
+    public static void main( String[] args ){
+
+        System.out.println("David");
+        System.out.println( "Hello World!" );
+
     }
 
 }
