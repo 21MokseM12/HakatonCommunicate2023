3fe2771ca5d83e0d78955f541b4bcb97e0687e88
dmitr <dmitriyegorov111@mail.ru>
2023-11-10 16:05:10 +0300

    fill DataPreparation class

---
+++ Hakaton2023/src/main/java/org/spring/mvcapp/App.java
@@ -0,0 +1,17 @@
+package org.spring.mvcapp;
+
+import config.DataPreparation;
+
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App{
+    public static void main( String[] args ) {
+        DataPreparation.setParameters();
+    }
+
+}
