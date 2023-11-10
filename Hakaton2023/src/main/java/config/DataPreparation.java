e1148189739abbee9953d22a161e2d1da0a39bc9
dmitr <dmitriyegorov111@mail.ru>
2023-11-10 13:16:20 +0300

    create DataPreparation class

---
+++ Hakaton2023/src/main/java/config/DataPreparation.java
@@ -0,0 +1,11 @@
+package config;
+
+final public class  DataPreparation {
+    public static final DataPreparation INSTANCE = new DataPreparation();
+    private double lat;
+    private double lon;
+    private int radius_meters;
+    private static final String API_KEY = "e0653b95af4751561059fedf25757f425fc507a4";
+    private static final String API_SECRET_KEY = "eb189f423a6fdba7a381430d72ce86205d07a305";
+
+}
