697afd1bbc82db85064267eb8644f5bf4d1ad99b
21MokseM12 <maksim.buynov.00@mail.ru>
2023-11-10 13:34:18 +0300

    configuration commit

--- Hakaton2023/src/main/java/org/spring/mvcapp/App.java
+++ Hakaton2023/src/main/java/org/spring/mvcapp/App.java
@@ -1,15 +1,11 @@
 package org.spring.mvcapp;
 
-/**
- * Hello world!
- *
- */
-
 public class App{
     public static void main( String[] args ){
 
         System.out.println("David");
         System.out.println( "Hello World!" );
+
     }
 
 }
