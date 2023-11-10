91b6fddc375fa36756c525a125e2fbd0273f2fb5
dmitr <dmitriyegorov111@mail.ru>
2023-11-10 13:17:58 +0300

    update access modifiers

--- Hakaton2023/src/main/java/config/DataPreparation.java
+++ Hakaton2023/src/main/java/config/DataPreparation.java
@@ -5,7 +5,7 @@ final public class  DataPreparation {
     private double lat;
     private double lon;
     private int radius_meters;
-    private static final String API_KEY = "e0653b95af4751561059fedf25757f425fc507a4";
-    private static final String API_SECRET_KEY = "eb189f423a6fdba7a381430d72ce86205d07a305";
+    public static final String API_KEY = "e0653b95af4751561059fedf25757f425fc507a4";
+    public static final String API_SECRET_KEY = "eb189f423a6fdba7a381430d72ce86205d07a305";
 
 }
