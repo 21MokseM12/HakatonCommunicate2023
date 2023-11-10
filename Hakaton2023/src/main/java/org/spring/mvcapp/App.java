d6f331b6938f6cae56341f864ebdf391e9884385
David <david.stepanyan2014@mail.ru>
2023-11-10 12:30:54 +0300

    added sout david

--- Hakaton2023/src/main/java/org/spring/mvcapp/App.java
+++ Hakaton2023/src/main/java/org/spring/mvcapp/App.java
@@ -4,10 +4,14 @@ package org.spring.mvcapp;
  * Hello world!
  *
  */
+
 public class App 
 {
     public static void main( String[] args )
     {
+
+        System.out.println("David");
         System.out.println( "Hello World!" );
     }
+
 }
