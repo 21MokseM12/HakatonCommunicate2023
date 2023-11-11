7e752a45bf2ce5b956e5c8eed99b18cb1bf22bd5
dmitr <dmitriyegorov111@mail.ru>
2023-11-11 17:25:05 +0300

    created a class for recording data in an Excel spreadsheet ExcelDataOutput

--- src/main/java/org/application/App.java
+++ src/main/java/org/application/App.java
@@ -2,10 +2,15 @@ package org.application;
 
 import config.DataPreparation;
 import service.ApiDadata;
+import service.ExcelDataOutput;
+
+import java.io.FileNotFoundException;
 
 public class App{
-    public static void main( String[] args ){
-        DataPreparation.setParameters();
-        ApiDadata.getAddress(DataPreparation.lat, DataPreparation.lon, DataPreparation.radius_meters);
+    public static void main( String[] args ) throws FileNotFoundException {
+        ExcelDataOutput excel = ExcelDataOutput.getInstance();
+        excel.write("127567", "Иваново", "Lybimova", "14");
+        excel.write("777777", "Armavir", "Moskovskay", "17");
+        excel.save();
     }
 }
